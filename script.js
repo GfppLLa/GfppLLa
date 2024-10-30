4b6e9e9f1c3ddef27bea73021da8c6e0bea75502
Eu, de Paula <92612598+GfppLLa@users.noreply.github.com>
2024-10-30 04:13:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("actionButton").addEventListener("click", function () {
+    console.log("Botão clicado! Execute a ação desejada aqui.");
+    alert("Ação realizada com sucesso!");
+});
